d83059c93b97ae245a0ad3bf5109a07a60c7f669
Ju-Viole-Grace <62285889+Ju-Viole-Grace@users.noreply.github.com>
2020-03-22 12:40:51 +0900

    Update 디스코드 봇.py

--- 디스코드 봇.py
+++ 디스코드 봇.py
@@ -1,5 +1,4 @@
 import discord
-import os
 client = discord.Client()
 
 
@@ -34,5 +33,4 @@ async def on_message(message):
         await message.channel.send("~~**많고 많은 마법사 들중 내가 제일 잘났 지**~~")
     if message.content.startswith("!많고많은마법사들중내가제일잘났지"):
         await m_essage.channel.send("그런 건 상대편한테나 하라고. ***처형시키기 전에.***")
-access_token = environ.os[*BOT_TOKEN*]
-client.run(access_token)
+client.run(NjkwMDkyMjY2MDUxNjAwNTcx.XnbdpA.UHAt9RHo65-r_OkK5XibaYTI860)
